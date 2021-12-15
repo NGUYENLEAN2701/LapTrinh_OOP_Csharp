b8a09bab49bf9c5431207d7e9c982720571a9658
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-12-15 16:22:13 +0700

    ok

--- Chuong_1/SS1/Hello_World/Program.cs
+++ Chuong_1/SS1/Hello_World/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (true) { Console.WriteLine("Hello, World"); }
---
+++ Chuong_1/SS1/Input_Output/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.InputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Hello World!");
+Console.Write("Xin Nhập Tên Bạn: ");
+var Name = Console.ReadLine();
+Console.WriteLine("Chào Mừng {0} Đến Với C#", Name);
+var ps = new ProcessStartInfo("https://nguyenlean.com/index.php")
+{
+    UseShellExecute = true,
+    Verb = "open"
+};
+//Process.Start(ps);
