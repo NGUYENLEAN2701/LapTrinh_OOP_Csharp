4076499bc239e93b9863a171cbef4c1194c52644
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-12-16 15:06:31 +0700

    ok csharp

---
+++ Chuong_1-LapTrinhHuongDoiTuong/SS2-Code_Example/Csharp_Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Green;
+
+Console.WriteLine("Hello, World!");
+S_TamGiac.s_tamgiac();
+Console.WriteLine("Bạn có muốn xem website: nguyenlean.com?");
+Console.Write("1-Có | 2-Không -> Chọn: ");
+int ch = 1;
+while (!int.TryParse(Console.ReadLine(), out ch))
+{
+    Console.Write("Bạn nhập sai - thử chọn lại: ");
+}
+if (ch == 1)
+{
+    var ps = new ProcessStartInfo("https://nguyenlean.com/index.php")
+    {
+        UseShellExecute = true,
+        Verb = "open"
+    };
+    Process.Start(ps);
+}
+else
+{
+    Console.WriteLine("Hẹn Gặp Lại!");
+    Console.ReadKey();
+}
---
+++ Chuong_1-LapTrinhHuongDoiTuong/SS2-Code_Example/Csharp_Example/S_TamGiac.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Green
+{
+    class S_TamGiac
+    {
+        public static void s_tamgiac()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Xin nhập độ dài 3 cạnh của tam giác: ");
+            Console.Write("a = ");
+            float a = 0;
+            while (!float.TryParse(Console.ReadLine(), out a))
+            {
+                Console.Write("Bạn nhập sai - thử lại: a = ");
+            }
+            Console.Write("b = ");
+            float b = 0;
+            while (!float.TryParse(Console.ReadLine(), out b))
+            {
+                Console.Write("Bạn nhập sai - thử lại: b = ");
+            }
+            Console.Write("c = ");
+            float c = 0;
+            while (!float.TryParse(Console.ReadLine(), out c))
+            {
+                Console.Write("Bạn nhập sai - thử lại: c = ");
+            }
+            //tính S tam giác:
+            float p = (a + b + c) / 2;
+            float s = (float)Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine("Diện Tích Tam Giác là: S = {0:0.00}", s);
+        }
+    }
+}
