29716f218e6e9c5f6c2f6d9f02c635adb3e78dcd
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2021-12-30 00:09:53 +0700

    ok

--- Chuong_1-LapTrinhHuongDoiTuong/SS3-CauTruc_va_OOP/ss3.2_OOP/5_Khai_Niem/5_Object/Class_box_Objects.cs
+++ Chuong_1-LapTrinhHuongDoiTuong/SS3-CauTruc_va_OOP/ss3.2_OOP/5_Khai_Niem/5_Object/Class_box_Objects.cs
@@ -21,6 +21,8 @@ namespace BoxApplication
         public double Breadth { get; set; }
         public double Height { get; set; }
 
+        private double _v_box;
+
         //! default constructor
         public Box()
         { }
@@ -38,18 +40,28 @@ namespace BoxApplication
         {
             Console.WriteLine("Length = {0}, Breadth = {1}, Height = {2}, private : {3}", Length, Breadth, Height, _length);
         }
+
+        public void V_box()
+        {
+            _v_box = _length * Breadth * Height;
+            Console.WriteLine("Thể Tích = {0} ", _v_box);
+        }
     }
 
     class Boxtester
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
             //! box1 is an object
             Box box1 = new Box(6.0, 7.0, 5.0);
 
             box1.Print();
             box1.Length = 5;
             Console.WriteLine("{0}", box1.Length);
+            box1.Print();
+            box1.V_box();
             Console.ReadKey();
         }
     }
