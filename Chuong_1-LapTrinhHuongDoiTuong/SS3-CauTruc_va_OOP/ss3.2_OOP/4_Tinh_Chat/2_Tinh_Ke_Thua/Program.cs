89b9afa83fc303930809165a32aa3eb732ce266e
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2022-01-04 01:13:48 +0700

    ok

---
+++ Chuong_1-LapTrinhHuongDoiTuong/SS3-CauTruc_va_OOP/ss3.2_OOP/4_Tinh_Chat/2_Tinh_Ke_Thua/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InheritanceApplication
+{
+    class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+
+    // Derived class
+    class Rectangle : Shape
+    {
+        public int GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public bool IsSquare()
+        {
+            if (Width == Height) return true;
+            else return false;
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            rectangle.Width = 5;
+            rectangle.Height = 7;
+
+            // Print the area of the object.
+            Console.WriteLine("Total area: {0}", rectangle.GetArea());
+
+            Square square = new Square();
+            square.Width = 9;
+            square.Height = 9;
+            Console.WriteLine("Total area: {0}", square.GetArea());
+            Console.WriteLine("Check is Square: {0}", square.IsSquare());
+            Console.ReadKey();
+        }
+    }
+}
