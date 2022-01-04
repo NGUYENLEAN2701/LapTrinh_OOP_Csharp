b20136283c49666d7ac0bb30874e6d4db0d858e3
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2022-01-04 18:41:26 +0700

    ok

---
+++ Chuong_1-LapTrinhHuongDoiTuong/SS3-CauTruc_va_OOP/ss3.2_OOP/4_Tinh_Chat/4_Tinh_Truu_Tuong/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AbstractionApplication
+{
+    //! Abstraction with interface
+    interface IExportData
+    {
+        void Export();
+    }
+
+    class TextExport : IExportData
+    {
+        public void Export()
+        {
+            Console.WriteLine("Export data to .txt file");
+        }
+    }
+
+    class CsvExport : IExportData
+    {
+        public void Export()
+        {
+            Console.WriteLine("Export data to .csv file");
+        }
+    }
+
+    class Application
+    {
+        private readonly IExportData _exportData;
+
+        public Application(IExportData exportData)
+        {
+            _exportData = exportData;
+        }
+
+        public void ExportData()
+        {
+            _exportData.Export();
+        }
+    }
+
+    class RectangleTester
+    {
+        static void Main(string[] args)
+        {
+            Application app = new Application(new TextExport());
+            app.ExportData();
+
+
+            Application app_cvs = new Application(new CsvExport());
+            app_cvs.ExportData();
+
+            Console.ReadKey();
+        }
+    }
+}
