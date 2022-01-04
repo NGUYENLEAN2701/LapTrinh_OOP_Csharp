43dfad1d59267a0c7dd0ae7dd5f9d9292d716931
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2022-01-05 01:13:33 +0700

    s

---
+++ Chuong_1-LapTrinhHuongDoiTuong/SS4-MoRong_Cpp_C#/Sort_Array_Cpp/Sort_Array.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>   //for time()
+#include <stdlib.h> //for srand
+void nhapsl_auto(int n, float *x)
+{
+    srand(time(0));
+    for (int i = 1; i <= n; i++)
+    {
+        x[i] = (float)(rand() % 10000 - 5000) / 100;
+    }
+}
+void sortArray(int n, float *x)
+{
+    for (int i = 1; i <= n; i++)
+        for (int j = i + 1; j <= n; j++)
+            if (x[i] > x[j])
+            {
+                float tg = x[i];
+                x[i] = x[j];
+                x[j] = tg;
+            }
+}
+void showArray(int n, float *x)
+{
+    printf("\n");
+    for (int i = 1; i <= n; i++)
+        printf("\n%0.2f", x[i]);
+}
+int main(void)
+{
+    int n;
+    printf("\nSo Phan Tu Cua Day N = ");
+    scanf("%d", &n);
+    float *x = (float *)malloc((n + 1) * sizeof(float));
+    nhapsl_auto(n, x);
+    showArray(n, x);
+    sortArray(n, x);
+    showArray(n, x);
+    return 0;
+}
