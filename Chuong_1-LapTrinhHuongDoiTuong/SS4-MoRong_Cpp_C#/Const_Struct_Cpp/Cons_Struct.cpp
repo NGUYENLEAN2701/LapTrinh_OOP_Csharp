eb805e106234686135500166d79ce994b8ea2943
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2022-01-05 11:22:28 +0700

    ok

--- Chuong_1-LapTrinhHuongDoiTuong/SS4-MoRong_Cpp_C#/Const_Struct_Cpp/Cons_Struct.cpp
+++ Chuong_1-LapTrinhHuongDoiTuong/SS4-MoRong_Cpp_C#/Const_Struct_Cpp/Cons_Struct.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <conio.h>
+typedef struct
+{
+    int x, y;
+    int color;
+} DIEM;
 
 int main()
 {
-
+    const DIEM p_zero = {0, 0, 255};
+    printf("\n%d", p_zero.x);
+    printf("\n%d", p_zero.y);
+    printf("\n%d", p_zero.color);
     return 0;
 }
